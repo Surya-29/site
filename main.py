008af48c492d67be3f4d9a2a724d6e467ad5aa9c
SK <suryakk001@gmail.com>
2022-08-17 15:42:35 +0530

    Added home page files

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template('index.html',the_title='么 Itnaava 么')
+
+# @app.route("/blog")
+# def blog_page():
+     
+app.run(debug=True)
