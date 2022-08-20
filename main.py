14d8472b11959f2563cda788e30a31780382ee23
SK <suryakk001@gmail.com>
2022-08-20 11:09:34 +0530

    modified:   main.py 	modified:   pages/.obsidian/workspace 	modified:   pages/blog/test.md 	modified:   templates/blog.html 	new file:   templates/new.html

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
-from flask import Flask,render_template
-# import markdown2
+from flask import Flask, render_template
+import markdown2
 import os
-
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template('index.html',the_title='么 Itnaava 么')
+    return render_template('index.html', the_title='么 Itnaava 么')
 
 @app.route("/blog")
 def blog_page():
-    l=list_dir('C://Users//surya//Desktop//Void//Antlia//么 Itnaava 么//pages//blog')
-    return render_template('blog.html',file_list=l)
-    
+    dir_lis = list_dir('pages/blog')
+    d={}
+    for i in dir_lis:
+        temp, article_info = md_to_html("pages/blog/"+i)
+        article_info['url']="/"+article_info['slug']
+        if (i[:-3]==article_info['slug']):
+            d[article_info['title']]=[article_info['date']]+[article_info['url']]
+    return render_template('blog.html', file_dict=d)
+
+@app.route("/blog/<url>")
+def md_test(url):
+    with open('templates/new.html', 'w') as f:
+        body, article_info = md_to_html("pages/blog/"+url+'.md')
+        body = "{% extends 'base.html' %} {% block body %}\n"+'<section class=post-date>' + \
+            article_info['date']+'</section>\n'+'<h1>'+article_info['title']+'</h1>\n' + \
+            '<h2 class="subtitle">' + \
+            article_info['subtitle']+'</h2>\n'+body+"\n{% endblock %}"
+        f.writelines(body)
+    return render_template('new.html')
+
 def list_dir(path):
-    dir_list=os.listdir(path)
+    dir_list = os.listdir(path)
     return dir_list
 
-# def md_to_html():
+def md_to_html(file_path):
+    with open(file_path) as f:
+        content = f.read()
+    html_cont = markdown2.markdown(
+        content, extras=['metadata', 'fenced-code-blocks', 'pyshell'])
+    meta_data = html_cont.metadata
+    return html_cont, meta_data
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- pages/blog/test.md
+++ pages/blog/test.md
@@ -4,21 +4,30 @@ template:
 
 slug: test
 
-title: 
+title: First blog
 
-subtitle: 
+subtitle: Just figuring out stuff
 
 date: 2022-08-19
 
 ---
 
-# Hello World
+This is the content of the page.
 
-#### Sample Heading
+just copy pasta ig
 
-```python 
+For all those struggling to get the formatting work even after trying the valid combinations in Visual Studio Code, don't forget to select the appropriate programming language type, it is at the bottom right in visual studio code window next to that smiley. Once you do that I found it works out of the box and you don't need any additional plugin to format code
+
+```python
 import math
 
 print(math.pi)
-
 ```
+
+How fast and static is this webpage?
+
+>Can it be rendered properly
+
+- Bulltein points
+- 1
+- 2
